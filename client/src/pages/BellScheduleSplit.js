7de12d399c4bfa427608240bc99e21266b13fddd
Stefan Lotz <stefanlotz07@gmail.com>
2024-07-06 17:44:25 -0400

    24-hour notation on bell schedule hover

--- client/src/pages/BellScheduleSplit.js
+++ client/src/pages/BellScheduleSplit.js
@@ -1,6 +1,8 @@
-import React from "react";
+import { React, useState } from "react";
 
 const BellScheduleSplit = ({ scheduleInfo }) => {
+  const [hoveredRowIndex, setHoveredRowIndex] = useState(null);
+
   function formatTime(time) {
     let [hours, minutes] = time.split(":");
     hours = parseInt(hours);
@@ -15,6 +17,14 @@ const BellScheduleSplit = ({ scheduleInfo }) => {
     return `${hours}:${minutes < 10 ? "0" + minutes : minutes}`;
   }
 
+  const handleMouseEnter = (index) => {
+    setHoveredRowIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredRowIndex(null);
+  };
+
   return (
     <div className="truncate">
       <table className="w-full max-w-sm m-auto dark:text-neutral-200">
@@ -22,11 +32,21 @@ const BellScheduleSplit = ({ scheduleInfo }) => {
           {scheduleInfo.periods?.map((period, index) => (
             <tr
               key={index}
-              className={index % 2 === 0 ? "bg-gray-100 dark:bg-neutral-600" : "bg-white dark:bg-neutral-700"}
+              className={
+                index % 2 === 0
+                  ? "bg-gray-100 hover:bg-gray-200 dark:bg-neutral-600 dark:hover:bg-neutral-500"
+                  : "bg-white hover:bg-gray-200 dark:bg-neutral-700 dark:hover:bg-neutral-500"
+              }
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
             >
               <td className="px-4 py-1.5">{period.name}</td>
               <td className="px-4 py-1.5 text-right">
-                {formatTime(period.startTime)} - {formatTime(period.endTime)}
+                {hoveredRowIndex === index
+                  ? `${period.startTime} - ${period.endTime}`
+                  : `${formatTime(period.startTime)} - ${formatTime(
+                      period.endTime
+                    )}`}
               </td>
             </tr>
           ))}
